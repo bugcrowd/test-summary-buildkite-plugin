54e1878ba8605122ec55f1cfa5527dfd15231420
tessereth <teresa.bradbury@bugcrowd.com>
2018-04-26 09:24:50 +1000

    Add tests to buildkite pipeline

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 HASH=$(git rev-parse HEAD)
-sed "s/@VERSION@/$HASH/g" "$SCRIPT_DIR/pipeline-sample.yml"
+sed "s/@VERSION@/$HASH/g" "$SCRIPT_DIR/pipeline.yml"
